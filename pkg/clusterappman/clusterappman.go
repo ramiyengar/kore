ab0df86204849e704c551e865f4bec622f142c48
Lewis Marshall <lewis.marshall@appvia.io>
2020-02-28 18:28:45 +0000

    update application controller

--- pkg/clusterappman/clusterappman.go
+++ pkg/clusterappman/clusterappman.go
@@ -176,7 +176,7 @@ func (s clusterappmanImpl) Run(ctx context.Context) error {
 				return fmt.Errorf("failed creating namespace %s: %s", m.Namespace, err)
 			}
 		}
-		// Write all objects to the API on a separate thread...
+		// TODO: Write all objects to the API on a separate thread...
 		if err := ca.CreateOrUpdate(ctx, m.Namespace); err != nil {
 			return fmt.Errorf("failed to create or update '%s' deployment: %s", m.Name, err)
 		}
@@ -197,17 +197,20 @@ func (s clusterappmanImpl) Run(ctx context.Context) error {
 		return fmt.Errorf("error reporting status: %s", err)
 	}
 
+	// now gather up all the component slices as they complete...
+	logger.Infof("waiting for %d cluster apps", len(components))
 	wg.Wait()
+	logger.Debug("wait group ended, now closing channels...")
 	close(ch)
 
-	// now gather up all the component slices as they complete...
-	logger.Infof("waiting for %d cluster apps", len(mm))
-	for i := range cs {
-		// get the first component "reason"
+	for i := range components {
+		// get the first component "status"
+		logger.Debugf("getting cluster app status %d of %d", i, len(components))
 		c := <-ch
+		logger.Debugf("received channel update %d for %s", i, c.Name)
 		// get the corresponding app
 		ca, ok := cas[c.Name]
-		// overwrite the component with the received updated "reason"
+		// overwrite the component with the received updated "status"
 		ca.Component = c
 		if !ok {
 			return fmt.Errorf("missing application from component -  %s", c.Name)
@@ -216,7 +219,7 @@ func (s clusterappmanImpl) Run(ctx context.Context) error {
 			logger.Infof("cluster app '%s' is ready", ca.Component.Name)
 		} else {
 			logger.Errorf(
-				"cluster app '%s' has timed out and has status [%s] due to [%s]",
+				"cluster app '%s' has timed out or failed and has status [%s] due to [%s]",
 				ca.Component.Name,
 				ca.Component.Status,
 				ca.Component.Detail,
@@ -224,7 +227,7 @@ func (s clusterappmanImpl) Run(ctx context.Context) error {
 		}
 		cs[i] = ca.Component
 	}
-	logger.Logger.Infof("saving status")
+	logger.Logger.Infof("saving final status")
 	if err := createStatusConfig(ctx, s.RuntimeClient, components); err != nil {
 		return fmt.Errorf("error reporting status: %s", err)
 	}
--- pkg/clusterappman/manifests/application-controller/README.md
+++ pkg/clusterappman/manifests/application-controller/README.md
@@ -1,14 +1,13 @@
 See issue https://github.com/kubernetes-sigs/application/issues/141
 
-The manifests were taken from here:
+The manifests were taken from here (with the image updated as below):
 ```
 git clone git@github.com:appvia/application.git
-git checkout v0.8.1-patched-image-version
 kubectl kustomize ./config/ > application-all.yaml
 ```
 
 The image referred to was generated from the source above with:
 ```
-docker build -t quay.io/appvia/application-controller:v0.8.1 .
-docker push quay.io/appvia/application-controller:v0.8.1
+docker build -t quay.io/appvia/application-controller:$(git rev-parse --short HEAD) .
+docker push quay.io/appvia/application-controller:$(git rev-parse --short HEAD) .
 ```
